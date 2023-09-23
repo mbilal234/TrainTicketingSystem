ec4b77856de05a3e88c227f68afedf50d2a306dd
Abdul Arham <abdul.arham9123@gmail.com>
2023-09-23 13:25:15 +0500

    commented fillCollections.py

--- fillCollections.py
+++ fillCollections.py
@@ -1,6 +1,7 @@
 """
     The purpose of this script is to fill data in the collections.
     This data includes trains, seats and fair prices.
+    The code script will be written after schema approval
 """
 
 import pymongo
