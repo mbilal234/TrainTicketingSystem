9253286495cf2102e98a2a22af80bde8afce164f
Abdul Arham <abdul.arham9123@gmail.com>
2023-09-24 20:12:26 +0500

    Added code for inserting documents to database and modified ERD

--- src/db/DatabaseConnection.py
+++ src/db/DatabaseConnection.py
@@ -8,10 +8,12 @@ import pymongo
 from dotenv import load_dotenv
 import os
 
-load_dotenv
+load_dotenv()
 
 my_client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = my_client["train_reservation_system"]
 
-print("Database Successfully Connected")
+if __name__=="__main__":
+    print("Database Successfully Connected")
 
 
