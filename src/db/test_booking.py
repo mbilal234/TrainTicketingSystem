3a4ceba9a4885f3e3f67927499960e3a213a7221
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-15 12:24:17 +0500

    added the specification for test_booking

--- src/db/test_booking.py
+++ src/db/test_booking.py
@@ -5,13 +5,22 @@ import unittest
 from datetime import datetime
 
 class TestBooking(unittest.TestCase):
+    """
+    Test cases for the booking functionality of the Train Reservation System.
+    """
 
     @classmethod
     def setUpClass(self):
+        """
+        Set up the necessary resources for testing the booking functionalities.
+        """
         self.db = db.DatabaseFunction()
 
     @classmethod
     def tearDownClass(self):
+        """
+        Clean up resources after testing the booking functionalities.
+        """
         self.db.bookings.delete_one({"bookingId": 3896})
         self.db.seats.update_many({"bookingId": 3896}, {"$set":{"bookingId": None}})
 
@@ -19,6 +28,9 @@ class TestBooking(unittest.TestCase):
         self.db.seats.update_many({"bookingId": 3897}, {"$set":{"bookingId": None}})
 
     def test_fare(self):
+        """
+        Test fare calculation for different routes and classes.
+        """
         fare = self.db.get_fare("Karachi", "Lahore", "economy")
         self.assertEqual(fare, 3500)
 
@@ -38,12 +50,11 @@ class TestBooking(unittest.TestCase):
         self.assertEqual(fare, 3400)
 
     def test_get_seats_and_time(self):
+        """
+        Test the retrieval of available seats and departure times for different routes and classes.
 
-        """
-        The trains we are using in this test case
-        has all free seats for the given time
-        """
-            
+        The trains used in this test case have all free seats for the given time.
+        """ 
         seats_and_time = self.db.get_seats_and_time("Karachi", "Rawalpindi", "2023-12-24", "08:00", "economy")
         economy_seats = self.db.seats.find({"travelId": 1080, "class":"economy"})
         expected_result = {"suggested":"2023-12-24 12:30:00", "times": {"2023-12-24 12:30:00": 1080, "2023-12-24 20:30:00":1081}, "seats":list(economy_seats)}
@@ -60,7 +71,9 @@ class TestBooking(unittest.TestCase):
         self.assertEqual(seats_and_time, actual_result)
 
     def test_get_business_seats(self):
-
+        """
+        Test the retrieval of available business class seats for different routes.
+        """
         economy_seats = []
         for i in range(100):
             economy_seats.append(str(i+1))
@@ -93,7 +106,9 @@ class TestBooking(unittest.TestCase):
         self.assertEqual(doc, expected)
 
     def test_business_booking(self):
-
+        """
+        Test the booking of business class seats.
+        """
         self.db.get_fare("Karachi", "Lahore", "business")
 
         doc = self.db.book_ticket("1234567891011", "Abdul Arham", 1000, "business", "A", "2002-09-17", 2, 0, 0, 0)
