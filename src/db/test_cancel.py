748ab37351779baba3202958d66f6ecbc656b99d
Hassaan Qaisar <hassaanqaisar2@gmail.com>
2023-12-15 12:30:43 +0500

    added the specification for test_cancel

--- src/db/test_cancel.py
+++ src/db/test_cancel.py
@@ -4,12 +4,20 @@ from DatabaseFunctions import DatabaseFunction
 from datetime import datetime
 
 class TestCancel(unittest.TestCase):
-
+    """
+    Test cases for the cancellation functionality of the Train Reservation System.
+    """
     @classmethod
     def setUpClass(self):
+        """
+        Set up the necessary resources for testing the cancellation functionalities.
+        """
         self.db = DatabaseFunction()
 
     def test_economy_cancel(self):
+        """
+        Test the cancellation of an economy class booking.
+        """
         self.db.get_fare("Karachi", "Lahore", "economy")
         self.db.book_ticket("1234567891011", "Abdul Arham", 1000, "economy", "", "2002-09-17", 3, 0, 0, 0)
 
@@ -20,6 +28,9 @@ class TestCancel(unittest.TestCase):
         self.assertEqual(doc, expected)
 
     def test_business_cancel(self):
+        """
+        Test the cancellation of a business class booking.
+        """
         self.db.get_fare("Karachi", "Lahore", "business")
         self.db.book_ticket("1234567891011", "Abdul Arham", 1000, "business", "A", "2002-09-17", 2, 0, 0, 0,)
 
@@ -30,6 +41,9 @@ class TestCancel(unittest.TestCase):
         self.assertEqual(doc, expected)
 
     def test_cancel_no_booking(self):
+        """
+        Test the case where no booking is found for cancellation.
+        """
         doc = self.db.cancel_booking(4333, "2345678901011")
 
         expected = "No booking found with this cnic and bookingId"
